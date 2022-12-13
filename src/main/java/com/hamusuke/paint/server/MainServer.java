5e165648fae20122fb40c4cb29016ee87505dc5d
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2022-12-14 07:16:42 +0900

    set port from arguments

--- src/main/java/com/hamusuke/paint/server/MainServer.java
+++ src/main/java/com/hamusuke/paint/server/MainServer.java
@@ -8,7 +8,15 @@ public class MainServer {
     private static final Logger LOGGER = LogManager.getLogger();
 
     public static void main(String[] args) {
-        final DedicatedPaintServer server = PaintServer.startServer(thread -> new DedicatedPaintServer(thread, "localhost", 8080));
+        int i = 8080;
+        if (args.length > 0) {
+            try {
+                i = Integer.parseInt(args[0]);
+            } catch (NumberFormatException ignored) {
+            }
+        }
+        int port = i;
+        final DedicatedPaintServer server = PaintServer.startServer(thread -> new DedicatedPaintServer(thread, "localhost", port));
         Thread thread = new Thread(() -> server.stop(true), "Server Shutdown Thread");
         thread.setUncaughtExceptionHandler((t, e) -> LOGGER.error("Caught exception", e));
         Runtime.getRuntime().addShutdownHook(thread);
