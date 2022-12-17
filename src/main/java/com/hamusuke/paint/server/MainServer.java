228d054d3321339b6ce414e65a9626e93ae5750b
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2022-12-17 18:28:08 +0900

    some changes

--- src/main/java/com/hamusuke/paint/server/MainServer.java
+++ src/main/java/com/hamusuke/paint/server/MainServer.java
@@ -4,19 +4,30 @@ import com.hamusuke.paint.server.dedicated.DedicatedPaintServer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class MainServer {
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
+        String s = InetAddress.getLocalHost().getHostAddress();
         int i = 8080;
-        if (args.length > 0) {
-            try {
-                i = Integer.parseInt(args[0]);
-            } catch (NumberFormatException ignored) {
+        for (String arg : args) {
+            if (arg.contains(":")) {
+                String[] KV = arg.split(":");
+                switch (KV[0]) {
+                    case "address":
+                        s = KV[1];
+                        break;
+                    case "port":
+                        i = Integer.parseInt(KV[1]);
+                }
             }
         }
+        String host = s;
         int port = i;
-        final DedicatedPaintServer server = PaintServer.startServer(thread -> new DedicatedPaintServer(thread, "localhost", port));
+        final DedicatedPaintServer server = PaintServer.startServer(thread -> new DedicatedPaintServer(thread, host, port));
         Thread thread = new Thread(() -> server.stop(true), "Server Shutdown Thread");
         thread.setUncaughtExceptionHandler((t, e) -> LOGGER.error("Caught exception", e));
         Runtime.getRuntime().addShutdownHook(thread);
